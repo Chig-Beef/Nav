3fba799d64e40b7f0029f1b1c65d8657ce6e40e6
Chig-Beef <rhystobyharris@gmail.com>
2024-12-13 15:50:27 +1030

    Started lexer

--- src/Lexer.c
+++ src/Lexer.c
@@ -1,4 +1,13 @@
+#include "Panic.h"
+#include "Token.h"
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+NEW_LIST_TYPE(Token, Token)
+
 typedef struct Lexer {
+  char *sourceName;
   char *source;
   int len;
   int index;
@@ -6,9 +15,30 @@ typedef struct Lexer {
 } Lexer;
 
 char peekChar(Lexer *l) {
-  if (l->index >= l->len - 1) {
+  int nextIndex = l->index + 1;
+
+  if (nextIndex >= l->len) {
     return 0;
   }
 
-  return l->source[l->index + 1];
+  return l->source[nextIndex];
+}
+
+void throwError(Lexer *l, char caller[], int line, char expected[], char *got) {
+  printf("Error in the Lexer!\nWhen the Lexer was trying to decipher: "
+         "%s\nError found in file: %s\nOn line: %i\nExpected: %s\nGot: %s\n",
+         caller, l->sourceName, line, expected, got);
+  exit(1);
+}
+
+// Returns the tokens of the source in the lexer, the resulting list must be
+// destroyed
+TokenList lex(Lexer *l) {
+  TokenList tokens;
+  if (TokenListInit(&tokens, 8)) {
+    panic("Couldn't initialise token list.");
+  }
+  Token token;
+
+  return tokens;
 }
