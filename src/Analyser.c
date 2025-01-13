b8b223236fa2c1e40814f25bcc266b2192a0c0c5
Chig-Beef <rhystobyharris@gmail.com>
2025-01-13 16:35:31 +1030

    Started semantic analyser

--- src/Analyser.c
+++ src/Analyser.c
@@ -1,5 +1,29 @@
+#include "Ident.h"
 #include "Node.h"
 
 typedef struct Analyser {
+  // Source
   Node enums, structs, funcs;
+
+  // Defined variables, types, etc
+  Stack vars;
 } Analyser;
+
+void analyserInit(Analyser *a, Node enums, Node structs, Node funcs) {
+  a->enums = enums;
+  a->structs = structs;
+  a->funcs = funcs;
+  a->vars = (Stack){NULL, 0};
+}
+
+void analyseEnums(Analyser *a) {}
+
+void analyseStructs(Analyser *a) {}
+
+void analyseFuncs(Analyser *a) {}
+
+void analyse(Analyser *a) {
+  analyseEnums(a);
+  analyseStructs(a);
+  analyseFuncs(a);
+}
